ad402bfc21f5108b4b6676bfabf1ab1d3b260699
Nicolas Vallée <contact@invokify.dev>
2026-04-12 10:51:40 +0200

    fix next config for vercel

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
 import type { NextConfig } from 'next';
-import path from 'path';
 
 const nextConfig: NextConfig = {
-  outputFileTracingRoot: path.join(__dirname, '../../../'),
+  // Configuration options can be added here
 };
 
 export default nextConfig;
